e5c251293954d6af6e3cedcf65d8a84e3df7e213
Phúc Phạm <phamphuc0603@gmail.com>
2022-04-03 15:58:59 +0700

    implementing main UI added mainUI animator and 4 animations implemented btns interactions for triggering param in animator

--- Assets/Scripts/UI/MenuUIManager.cs
+++ Assets/Scripts/UI/MenuUIManager.cs
@@ -4,17 +4,30 @@ using UnityEngine;
 
 public class MenuUIManager : MonoBehaviour
 {
+    [SerializeField] private Animator menuUIAnim;
+
+    public static MenuUIManager Singleton { get; private set; }
+
+    private void Awake()
+    {
+        if (Singleton != null)
+            Singleton = this;
+    }
+
     public void OnLocalGameBtnClicked()
     {
         Debug.Log("Local Game");
+        menuUIAnim.SetTrigger("InGameUI");
     }
     public void OnOnlineGameBtnClicked()
     {
         Debug.Log("Online Game");
+        menuUIAnim.SetTrigger("UI-2");
     }
     public void OnHostBtnClicked()
     {
         Debug.Log("Host");
+        menuUIAnim.SetTrigger("UI-3");
     }
     public void OnConnectBtnClicked()
     {
@@ -23,10 +36,12 @@ public class MenuUIManager : MonoBehaviour
     public void OnBackUI2BtnClicked()
     {
         Debug.Log("Back UI 2");
+        menuUIAnim.SetTrigger("UI-1");
     }
 
     public void OnBackUI3BtnClicked()
     {
         Debug.Log("Back UI 3");
+        menuUIAnim.SetTrigger("UI-2");
     }
 }
