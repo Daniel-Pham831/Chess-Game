b9d10073784eb588f4e07e65bf095d7f46306284
Phúc Phạm <phamphuc0603@gmail.com>
2022-04-03 15:26:41 +0700

    Implementing Menu UI added MenuUIManger for controlling menu ui added menu ui btns layout

---
+++ Assets/Scripts/UI/MenuUIManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuUIManager : MonoBehaviour
+{
+    public void OnLocalGameBtnClicked()
+    {
+        Debug.Log("Local Game");
+    }
+    public void OnOnlineGameBtnClicked()
+    {
+        Debug.Log("Online Game");
+    }
+    public void OnHostBtnClicked()
+    {
+        Debug.Log("Host");
+    }
+    public void OnConnectBtnClicked()
+    {
+        Debug.Log("Connect");
+    }
+    public void OnBackUI2BtnClicked()
+    {
+        Debug.Log("Back UI 2");
+    }
+
+    public void OnBackUI3BtnClicked()
+    {
+        Debug.Log("Back UI 3");
+    }
+}
